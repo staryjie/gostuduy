f8140589dafc107f3ab965f3bd46172e05e5103f
staryjie <staryjie@163.com>
2022-01-08 23:27:26 +0800

    Go常用命令和注释

---
+++ listen1/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 单行注释
+	fmt.Println("hello world!")
+
+	/*
+		a := 1 + 3
+		b := 4 -1
+	*/
+
+	// go run   编译并运行go源文件
+	// go build 将源码编译成可执行的二进制文件
+	// go fmt   格式化Go代码
+
+	/*
+		多行注释的第一行
+		这是第二行注释
+	*/
+}
