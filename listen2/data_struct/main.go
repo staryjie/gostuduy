b2ca9b95a097f40f546d3a40f8e9cf0f221fc73a
staryjie <staryjie@163.com>
2022-01-10 22:04:38 +0800

    整型

--- listen2/data_struct/main.go
+++ listen2/data_struct/main.go
@@ -4,7 +4,7 @@ package main
 
 import "fmt"
 
-func main() {
+func bool_demo() {
 	// 布尔型
 	var b bool // 没有赋值，默认值为false
 	fmt.Println(b)
@@ -33,4 +33,21 @@ func main() {
 
 	// 格式化输出
 	fmt.Printf("a = %t  b = %t\n", a, b)
+}
+
+func int_demo() {
+	var a int8
+	a = -128
+	fmt.Printf("a = %d\n", a)
+	a = 127
+	fmt.Printf("a = %d\n", a)
+
+	var b int
+	b = 123456
+	fmt.Printf("b = %d\n", b)
+}
+
+func main() {
+	// bool_demo()
+	int_demo()
 }
