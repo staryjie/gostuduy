c64657baa6f90a50b9c2714afd92b517e285b829
staryjie <staryjie@163.com>
2022-01-10 21:12:44 +0800

    数据类型之布尔类型

---
+++ listen2/data_struct/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b bool // 没有赋值，默认值为false
+	fmt.Println(b)
+
+	b = true // 赋值
+	fmt.Println(b)
+
+	b = !b // 取反
+	fmt.Println(b)
+
+	// && 与操作
+	var a bool = true
+
+	if a == true && b == true {
+		fmt.Println("Right")
+	} else {
+		fmt.Println("Wrong")
+	}
+
+	// || 或操作
+	if a == true || b == true {
+		fmt.Println("Right")
+	} else {
+		fmt.Println("Wrong")
+	}
+
+	// 格式化输出
+	fmt.Printf("a = %t  b = %t\n", a, b)
+}
