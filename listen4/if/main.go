46b49124367ef50628ebc9df2db79d2c9c8f071d
staryjie <staryjie@163.com>
2022-01-11 14:34:59 +0800

    if-else-if语句

--- listen4/if/main.go
+++ listen4/if/main.go
@@ -12,8 +12,25 @@ func if_expr(num int) string {
 	}
 }
 
+func if_else_if_expr(num int) {
+	if num > 5 && num <= 10 {
+		fmt.Println("5 < num <= 10")
+	} else if num > 0 && num <= 5 {
+		fmt.Println("0 < num <= 5")
+	} else {
+		fmt.Printf("num is %d\n", num)
+	}
+}
+
 func main() {
-	num := 11
-	res := if_expr(num)
-	fmt.Println(res)
+	num := 5
+
+	// if else 语句
+	/*
+		res := if_expr(num)
+		fmt.Println(res)
+	*/
+
+	// if else if else
+	if_else_if_expr(num)
 }
