55461ae239b2dc348500d7389d0302ac1cb5edca
staryjie <staryjie@163.com>
2022-01-11 21:29:22 +0800

    switch中初始化局部变量

--- listen4/switch/main.go
+++ listen4/switch/main.go
@@ -20,6 +20,24 @@ func switch_demo() {
 	}
 }
 
+func init_switch() {
+	switch a := 3; a {
+	case 1:
+		fmt.Println("a = 1")
+	case 2:
+		fmt.Println("a = 2")
+	case 3:
+		fmt.Println("a = 3")
+	case 4:
+		fmt.Println("a = 4")
+	case 5:
+		fmt.Println("a = 5")
+	default:
+		fmt.Printf("a = %d\n", a)
+	}
+}
+
 func main() {
-	switch_demo()
+	// switch_demo()
+	init_switch()
 }
