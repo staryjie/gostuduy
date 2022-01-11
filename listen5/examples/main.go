7ac94027e7cf836861949bd8be8de72825496314
staryjie <staryjie@163.com>
2022-01-11 22:46:55 +0800

    100-999之间的水仙花数

--- listen5/examples/main.go
+++ listen5/examples/main.go
@@ -26,6 +26,27 @@ func prime_number() {
 	}
 }
 
+func is_daffodils(n int) bool {
+	first := n % 10
+	second := (n / 10) % 10
+	third := (n / 100) % 10
+
+	if first*first*first+second*second*second+third*third*third == n {
+		return true
+	}
+	return false
+}
+
+func daffodils_number() {
+	// 100-999之间是水仙花数
+	for i := 100; i < 1000; i++ {
+		if is_daffodils(i) {
+			fmt.Printf("%d ", i)
+		}
+	}
+}
+
 func main() {
-	prime_number()
+	// prime_number()
+	daffodils_number()
 }
