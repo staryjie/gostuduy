d98502ee1b7fef25f22e64a97d1193e0a3614184
staryjie <staryjie@163.com>
2022-01-11 22:17:09 +0800

    必传参数和变长参数

--- listen5/func_mutil_args/main.go
+++ listen5/func_mutil_args/main.go
@@ -9,7 +9,8 @@ func mutil_args(a, b int) (sum, sub int) {
 	return
 }
 
-func calc(n ...int) (sum int) {
+func calc(a int, n ...int) (sum int) {
+	sum += a
 	for i := 0; i < len(n); i++ {
 		sum += n[i]
 	}
@@ -23,6 +24,7 @@ func main() {
 	// sum2, _ := mutil_args(19, 3)
 	// fmt.Printf("sum = %d\n", sum2)
 
-	sum := calc(1, 2, 3, 4, 5)
+	// sum := calc(1, 2, 3, 4, 5)
+	sum := calc(8, 1, 2, 3, 4, 5)  // a int 是必传的参数，至少需要传递一个参数
 	fmt.Printf("sum = %d\n", sum)
 }
