adf65711e6ebfc9e1937219eb054a35b388f648a
staryjie <staryjie@163.com>
2022-01-11 22:10:13 +0800

    匿名变量接收不需要使用的返回值

--- listen5/func_mutil_args/main.go
+++ listen5/func_mutil_args/main.go
@@ -12,4 +12,7 @@ func mutil_args(a, b int) (sum, sub int) {
 func main() {
 	sum, sub := mutil_args(12, 6)
 	fmt.Printf("sum = %d  sub = %d\n", sum, sub)
+
+	sum2, _ := mutil_args(19, 3)
+	fmt.Printf("sum = %d\n", sum2)
 }
