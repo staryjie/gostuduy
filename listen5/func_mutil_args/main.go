e49c9d8d1c9d37be791e16b659c66903b09eb97a
staryjie <staryjie@163.com>
2022-01-11 22:14:23 +0800

    可变参数

--- listen5/func_mutil_args/main.go
+++ listen5/func_mutil_args/main.go
@@ -9,10 +9,20 @@ func mutil_args(a, b int) (sum, sub int) {
 	return
 }
 
-func main() {
-	sum, sub := mutil_args(12, 6)
-	fmt.Printf("sum = %d  sub = %d\n", sum, sub)
+func calc(n ...int) (sum int) {
+	for i := 0; i < len(n); i++ {
+		sum += n[i]
+	}
+	return
+}
 
-	sum2, _ := mutil_args(19, 3)
-	fmt.Printf("sum = %d\n", sum2)
+func main() {
+	// sum, sub := mutil_args(12, 6)
+	// fmt.Printf("sum = %d  sub = %d\n", sum, sub)
+
+	// sum2, _ := mutil_args(19, 3)
+	// fmt.Printf("sum = %d\n", sum2)
+
+	sum := calc(1, 2, 3, 4, 5)
+	fmt.Printf("sum = %d\n", sum)
 }
