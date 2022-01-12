d5abfb7f91e1a21455d0b10a9eaf08e9a31d7ba1
staryjie <staryjie@163.com>
2022-01-12 10:10:09 +0800

    局部变量在代码块中

--- listen6/variable/main.go
+++ listen6/variable/main.go
@@ -12,8 +12,21 @@ func globalVariable() {
 }
 
 func localVariable() {
-	var b int = 99 // 局部变量
+	var b int = 99 // 局部变量,在函数内部
 	fmt.Printf("b = %d\n", b)
+
+	if b == 99 {
+		var c int = 98 // 局部变量,在语句块内
+		fmt.Printf("c = %d\n", c)
+	}
+
+	if d := 100; d > 0 { // 局部变量，在代码块中
+		fmt.Printf("d = %d\n", d)
+	}
+
+	for i := 0; i < 10; i++ { // 局部变量,在for循环中有效
+		fmt.Println(i)
+	}
 }
 
 func main() {
