ea81c447daff6569f1e372ae413387fb551f3fb9
staryjie <staryjie@163.com>
2022-01-13 10:23:15 +0800

    数组初始化时赋值

--- listen7/array/main.go
+++ listen7/array/main.go
@@ -11,6 +11,15 @@ func testArray1() {
 	fmt.Println(arr1)
 }
 
+func testArray2() {
+	// var arr2 [5]int = [5]int{1, 2, 3, 4, 5}
+	// arr2 := [5]int{1, 2, 3, 4, 5} // 类型推导初始化
+	// arr2 := [...]int{1, 2, 3, 4, 5} // 自动推导数组长度
+	arr2 := [5]int{1, 2, 3} // 初始化的时候给一部分元素赋值，其他元素为默认值
+	fmt.Println(arr2)
+}
+
 func main() {
-	testArray1()
+	// testArray1()
+	testArray2()
 }
