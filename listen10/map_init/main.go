cca72e0e0caa9c5386c33e254e6b4a317bc8221e
staryjie <staryjie@163.com>
2022-01-14 16:16:18 +0800

    map的初始化、修改和访问

---
+++ listen10/map_init/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var m map[string]int = map[string]int{ // map初始化
+		"stu01": 1000,
+		"stu02": 2000,
+		"stu03": 3000,
+	}
+
+	fmt.Println(m)
+
+	// 修改map中的元素
+	m["stu01"] = 1111
+	m["stu04"] = 4000
+
+	// 访问map中的元素
+	fmt.Printf("the value of m[stu04] is: %d\n", m["stu04"])
+}
