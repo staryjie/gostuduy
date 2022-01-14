6bf7a02ed5389404f65868a2e36b606f962be3a3
staryjie <staryjie@163.com>
2022-01-14 10:01:37 +0800

    切片的容量从切片开始位置开始计算

--- listen8/make/main.go
+++ listen8/make/main.go
@@ -37,6 +37,9 @@ func testCap() {
 	b := a[1:3]
 	fmt.Printf("len a = %d\ncap b = %d\n", len(b), cap(b)) // len = 2 cap = 7
 	// a的长度为8，a[1:3]只有两个元素，a[0]并不在b能操作的范围，所以cap = 8 -1 = 7
+
+	b = b[:cap(b)]
+	fmt.Printf("再切片：len b = %d\ncap b = %d\n", len(b), cap(b))
 }
 
 func main() {
