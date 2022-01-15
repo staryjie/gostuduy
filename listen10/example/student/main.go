a7ac4cbb27f35bf5f2ebcd12cc0d2a4b488ccf36
staryjie <staryjie@163.com>
2022-01-15 21:52:29 +0800

    优化代码显示

--- listen10/example/student/main.go
+++ listen10/example/student/main.go
@@ -16,14 +16,14 @@ func storeStudents(n int) {
 	rand.Seed(time.Now().UnixNano())
 	stuMap := make(map[int]Student, n)
 
-	for i := 1; i <= n; i++ {
+	for i := 11; i <= n+10; i++ {
 		id := i
 		stu := Student{Age: rand.Intn(15) + 10, Name: fmt.Sprintf("stu%02d", i), Score: float64(rand.Intn(60) + 40 + int(rand.Float64()))}
 		stuMap[id] = stu
 	}
 
 	for k, v := range stuMap {
-		fmt.Printf("id=%d's student info Name: %s\tAge: %d\tScore:%.2f\n", k, v.Name, v.Age, v.Score)
+		fmt.Printf("id=%d's student info\tName: %s\tAge: %d\tScore: %.2f\n", k, v.Name, v.Age, v.Score)
 	}
 }
 
