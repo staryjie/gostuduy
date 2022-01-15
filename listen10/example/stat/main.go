53d3c6a1d9bfcdb86a696dcfb5d61c07a3e1b639
staryjie <staryjie@163.com>
2022-01-15 21:23:55 +0800

    统计字符串中每个单词出现的次数

---
+++ listen10/example/stat/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func statWordCount(str string) map[string]int {
+	words := strings.Split(str, " ")
+
+	result := make(map[string]int, len(words))
+
+	for _, word := range words {
+		count, ok := result[word]
+		if !ok {
+			result[word] = 1
+		} else {
+			result[word] = count + 1
+		}
+	}
+	return result
+}
+
+func main() {
+	str := "how do you do ? are you kidding"
+	result := statWordCount(str)
+	fmt.Printf("result: %#v\n", result)
+}
