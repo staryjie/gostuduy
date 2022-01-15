b670a9a2f076b157293be3f08d87a9fbd1cd5ddf
staryjie <staryjie@163.com>
2022-01-15 22:46:05 +0800

    go语言代码执行顺序

--- listen11/calc_test/main.go
+++ listen11/calc_test/main.go
@@ -21,3 +21,13 @@ func main() {
 	sum := calc.Add(a, b)
 	fmt.Printf("sum = %d\n", sum)
 }
+
+/*
+	Go语言中代码执行顺序
+	按照导入包的先后顺序，逆序执行
+	1. 先执行全局变量的初始化
+	2. 执行对应的init函数
+	3. main包的全局变量初始化
+	4. main包init函数
+	5. main函数
+*/
