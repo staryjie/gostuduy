6abd40858c77081d79ce9d05a9329bfdade1046d
staryjie <staryjie@163.com>
2022-01-16 00:17:02 +0800

    结构体嵌套匿名结构体

---
+++ listen12/struct_nest/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Address struct {
+	Country  string
+	Province string
+	City     string
+}
+
+type User struct {
+	Username string
+	Age      int
+	Sex      int
+	int      // 匿名字段直接使用数据类型作为变量
+	string
+	Address // 结构体嵌套,直接使用结构体名做为匿名成员变量
+}
+
+func main() {
+	user := &User{
+		Username: "Tom",
+		Age:      20,
+		Sex:      1,
+		int:      12,
+		string:   "Hi",
+		Address:  Address{Country: "China", Province: "ZHejiang", City: "Hangzhou"},
+	}
+	fmt.Printf("user = %#v\n", user)
+}
