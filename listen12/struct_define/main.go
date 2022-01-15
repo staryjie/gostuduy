2d7a1348890f2b637045714c256dcc8e53841845
staryjie <staryjie@163.com>
2022-01-15 23:26:52 +0800

    结构体的定义，结构体实例声明、成员变量赋值和访问

---
+++ listen12/struct_define/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type User struct { // 结构体的定义
+	Username string
+	Sex      int
+	Age      int
+	AvataUrl string
+}
+
+func main() {
+	var user User // 声明结构体实例
+	// 对结构体实例成员属性赋值
+	user.Age = 18
+	user.AvataUrl = "https://img2020.cnblogs.com/blog/1188507/202111/1188507-20211116110743641-1021429203.png"
+	user.Sex = 1
+	user.Username = "Tom"
+
+	// 结构体实例成员属性访问
+	fmt.Printf("Username:%s age:%d sex:%d avataurl:%s\n", user.Username, user.Age, user.Sex, user.AvataUrl)
+}
