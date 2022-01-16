803e16862e3aa14e8640172a4725bffc3923c2ab
staryjie <staryjie@163.com>
2022-01-16 23:48:46 +0800

    要修改结构体实例中成员值，必须要传递结构体指针

--- listen13/method/main.go
+++ listen13/method/main.go
@@ -13,7 +13,16 @@ func (p People) PeopleInfo() {
 	fmt.Printf("%s is come from %s!\n", p.Name, p.Country)
 }
 
+func (p *People) Set(name, country string) {
+	p.Name = name
+	p.Country = country
+}
+
 func main() {
 	p1 := People{Name: "Tom", Country: "America"}
 	p1.PeopleInfo()
+
+	p2 := &People{Name: "Bob", Country: "America"}
+	p2.Set("Tim", "England")
+	p2.PeopleInfo()
 }
