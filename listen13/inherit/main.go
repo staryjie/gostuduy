f87847230efa38b38d643e54ff3cfe66c4c8858b
staryjie <staryjie@163.com>
2022-01-17 00:24:51 +0800

    方法冲突的时候，需要额外指定匿名字段名称

--- listen13/inherit/main.go
+++ listen13/inherit/main.go
@@ -16,19 +16,31 @@ func (a *Animal) Talk() {
 type Dog struct {
 	// Animal
 	*Animal // 实现"继承"
-	Feet    string
+	*BuruAnimal
+	Feet string
 }
 
 func (d *Dog) Eat() {
 	fmt.Println("Dog Eat.")
 }
 
+/*
 func (d *Dog) Talk() { // 重写父类方法
 	fmt.Println("wang wang!")
+}
+*/
+
+type BuruAnimal struct {
+	*Animal
+}
+
+func (b *BuruAnimal) Talk() {
+	fmt.Printf("Hi, I'm %s\n", b.Name)
 }
 
 func main() {
 	var dog *Dog = &Dog{Feet: "four legs", Animal: &Animal{Name: "小白", Sex: "雄性"}}
-	dog.Talk()
+	// dog.Talk()  // ambiguous selector dog.Talk
+	dog.Animal.Talk()
 	dog.Eat()
 }
