21fec2026a19b2920cc988aaaa6ee024955148b0
staryjie <staryjie@163.com>
2022-01-17 21:37:13 +0800

    defer底层原理

--- listen15/defer/main.go
+++ listen15/defer/main.go
@@ -39,3 +39,15 @@ func main() {
 	fmt.Println(funcC())
 	fmt.Println(funcD())
 }
+
+/*
+	defer执行原理
+	1. defer在函数返回之前被调用
+	2. return的实现
+		2.1 设置返回值变量的值
+		2.2 执行RET指令
+	3. defer原理
+		3.1 函数返回前设置返回变量的值
+		3.2 调用defer语句
+		3.3 执行RET指令
+*/
