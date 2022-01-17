4a7635eb686822244ecbfcfbe129fd7f417338e6
staryjie <staryjie@163.com>
2022-01-17 22:17:22 +0800

    接口的定义、实现和使用

---
+++ listen16/interface_demo/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type Animaler interface {
+	Talk()
+	Eat()
+	Name() string
+}
+
+type Dog struct {
+	name string
+}
+
+func (d Dog) Talk() {
+	fmt.Printf("%s: 汪汪汪\n", d.name)
+}
+
+func (d Dog) Eat() {
+	fmt.Printf("%s在啃骨头\n", d.name)
+}
+
+func (d Dog) Name() string {
+	return d.name
+}
+
+type Pig struct {
+	name string
+}
+
+func (p Pig) Talk() {
+	fmt.Printf("%s: 杠杠杠\n", p.name)
+}
+
+func (p Pig) Eat() {
+	fmt.Printf("%s在吃草\n", p.name)
+}
+
+func (p Pig) Name() string {
+	return p.name
+}
+
+func main() {
+	dog := Dog{name: "小白"}
+	dog.Talk()
+	dog.Eat()
+	fmt.Println(dog.Name())
+
+	var a Animaler
+	a = dog
+	a.Talk()
+	a.Eat()
+	fmt.Println(a.Name())
+
+	fmt.Println("----------")
+
+	pig := Pig{name: "小花"}
+	pig.Talk()
+	pig.Eat()
+	fmt.Println(pig.Name())
+}
