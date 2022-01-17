7d66586544ea9c7563ca761a0aa54f9584fc60db
staryjie <staryjie@163.com>
2022-01-17 10:37:56 +0800

    从字符串中获取输入

---
+++ listen14/str_input/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func testSscanf() {
+	var (
+		a int
+		s string
+		f float64
+	)
+	var str string = "88 hi 6.8"
+
+	fmt.Sscanf(str,"%d %s %f", &a, &s, &f)
+	fmt.Printf("a = %d  s = %s  f = %.2f\n", a, s, f)
+}
+
+func testSscan() {
+	var (
+		a int
+		s string
+		f float64
+	)
+	var str string = "88 hi\n\n 6.8"
+	fmt.Sscan(str, &a, &s, &f)
+	fmt.Printf("a = %d  s = %s  f = %.2f\n", a, s, f)
+}
+
+func testSscanln() {
+	var (
+		a int
+		s string
+		f float64
+	)
+	var str string = "88 hi\n\n 6.8"
+	fmt.Sscanln(str, &a, &s, &f)
+	fmt.Printf("a = %d  s = %s  f = %.2f\n", a, s, f)
+}
+
+func main() {
+	//testSscanf()
+	//testSscan()
+	testSscanln()
+}
