4783283e7cfa83ce5024f821269a5d2c2246fd5c
staryjie <staryjie@163.com>
2022-01-17 23:13:51 +0800

    通过switch type断言实现某接口的不同实例的类型

--- listen16/type_assert/main.go
+++ listen16/type_assert/main.go
@@ -7,6 +7,44 @@ type Student struct {
 	Sex int
 }
 
+type Animaler interface {
+	Talk()
+	Eat()
+	Name() string
+}
+
+type Dog struct {
+	name string
+}
+
+func (d Dog) Talk() {
+	fmt.Printf("%s: 汪汪汪\n", d.name)
+}
+
+func (d Dog) Eat() {
+	fmt.Printf("%s在啃骨头\n", d.name)
+}
+
+func (d Dog) Name() string {
+	return d.name
+}
+
+type Pig struct {
+	name string
+}
+
+func (p Pig) Talk() {
+	fmt.Printf("%s: 杠杠杠\n", p.name)
+}
+
+func (p Pig) Eat() {
+	fmt.Printf("%s在吃草\n", p.name)
+}
+
+func (p Pig) Name() string {
+	return p.name
+}
+
 func getType(i interface{}) {
 	if t, ok := i.(int); ok {
 		fmt.Printf("%T = %d\n", t, t)
@@ -43,6 +81,17 @@ func findType(i interface{}) {
 	}
 }
 
+func justAnimal(a Animaler) {
+	switch a.(type) {
+	case Dog:
+		fmt.Println("This is a Dog!")
+	case Pig:
+		fmt.Println("This is a Pig!")
+	default:
+		fmt.Printf("%T = %#v\n", a, a)
+	}
+}
+
 func main() {
 	i := 12
 	getType(i)
@@ -65,4 +114,14 @@ func main() {
 		Sex: 1,
 	}
 	findType(stu)
+
+	dog := Dog{
+		name: "小白",
+	}
+	justAnimal(dog)
+
+	pig := Pig{
+		name: "小花",
+	}
+	justAnimal(pig)
 }
