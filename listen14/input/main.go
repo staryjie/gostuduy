f0dd2a685c1469c5a66ddb431a2801f368010f39
staryjie <staryjie@163.com>
2022-01-17 09:55:29 +0800

    Scan函数

--- listen14/input/main.go
+++ listen14/input/main.go
@@ -17,6 +17,18 @@ func testScanf() {
 	fmt.Printf("a = %d  s = %s  f = %.2f\n", a, s, f)
 }
 
+func testScan() {
+	var (
+		a int
+		s string
+		f float64
+	)
+
+	fmt.Scan(&a, &s, &f)
+	fmt.Printf("a = %d  s = %s  f = %.2f\n", a, s, f)
+}
+
 func main() {
-	testScanf()
+	// testScanf()
+	testScan()
 }
