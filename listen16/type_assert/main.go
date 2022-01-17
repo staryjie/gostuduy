93f63c4321caef4a649ed63793e5e62fbe96435a
staryjie <staryjie@163.com>
2022-01-17 23:05:35 +0800

    通过switch type实现类型断言

--- listen16/type_assert/main.go
+++ listen16/type_assert/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type Student struct {
+	Name string
+	Sex int
+}
+
 func getType(i interface{}) {
 	if t, ok := i.(int); ok {
 		fmt.Printf("%T = %d\n", t, t)
@@ -23,16 +28,41 @@ func getType(i interface{}) {
 	}
 }
 
+func findType(i interface{}) {
+	switch i.(type) {
+	case int:
+		fmt.Printf("%T = %d\n", i, i)
+	case string:
+		fmt.Printf("%T = %s\n", i, i)
+	case float32:
+		fmt.Printf("%T = %f\n", i, i)
+	case float64:
+		fmt.Printf("%T = %f\n", i, i)
+	default:
+		fmt.Printf("%T = %#v\n", i, i)
+	}
+}
+
 func main() {
 	i := 12
 	getType(i)
-	
+	findType(i)
+
 	s := "hello"
 	getType(s)
+	findType(s)
 
 	var f64 float64 = 3.14
 	getType(f64)
+	findType(f64)
 
 	var f32 float32 = 4.17
 	getType(f32)
+	findType(f32)
+
+	stu := Student{
+		Name: "Tom",
+		Sex: 1,
+	}
+	findType(stu)
 }
